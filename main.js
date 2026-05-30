2d561c63abbb15bb21f95b56be4a17e039562217
Eleonora Losciuto <eleonora.losciuto@gmail.com>
2026-05-30 18:57:32 +0200

    JS fix: input extraction + event listener

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
-// Modifico comportamento di default del bottone submit
-const formButton = document.querySelector('button');
-formButton.addEventListener('click', x => x.preventDefault()); // note: to check with form and event submit
 
-// Salvo le informazioni in due variabili
+// estrazione form
+const form = document.getElementById('ticket');
+console.log(form); //check in console
 
-const birthDate = document.getElementById(birthDate);
-const km = document.getElementById(kmNum);
+//estrazione anno attuale
+const d = new Date;
+const currentYear = d.getFullYear();
+console.log(currentYear); // check in console
+
+
+form.addEventListener('submit', e => 
+    {   // prevent default all'evento submit (NO ricarica pagina)
+        e.preventDefault(); 
+        //Estrazione dati inseriti dall'utente - all'evento Submit
+        const birthYearInput = document.getElementById('birthDate').value; 
+        console.log(birthYearInput); // check in console
+        const kmNumInput = document.getElementById('kmNum').value;
+        console.log(kmNumInput); // check in console
+        // calcolo etá
+        const age = currentYear - birthYearInput;
+        console.log(`la persona ha ${age} anni`); // check in console
+    });
+
+
+
 
 
