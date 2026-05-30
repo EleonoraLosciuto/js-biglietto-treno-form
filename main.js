703efcd65a04860f524776737f13f55fad74949c
Eleonora Losciuto <eleonora.losciuto@gmail.com>
2026-05-30 21:32:35 +0200

    add controls for inputs shown on card

--- main.js
+++ main.js
@@ -15,28 +15,35 @@ const kmCard = document.getElementById('kmInput');
 const ageCard = document.getElementById('ageUser');
 const finalPriceCard = document.getElementById('finalPrice');
 
+//creo variabili globali con let per memorizzare gli input utente
+let age = null;
+let kmNumInput = 0;
 
 // EVENT LISTENER PER INPUT 
-// //prevent default per gli input e mostro l'input utente nella card
+// mostro l'input utente nella card
 
-birthYear.addEventListener('input', inp => {
-    inp.preventDefault()
+form.addEventListener('input', i => {
+    // check con taget per evitare che si compili "anni 2026" nella card --> nota: target "isola" lo specifico elemento sul quale l'utente sta digitando 
+    if (i.target.id === "birthDate") { // se utente sta digitando dentro l'input dell'anno di nascita
+        let birthYearInput = birthYear.value;
     
-    // calcolo etá
-    const birthYearInput = birthYear.value; 
-    const age = currentYear - birthYearInput;
+        if (birthYearInput.length === 4 && (currentYear - birthYearInput >= 0)) {  // se l'anno indicato é di 4 cifre e precedente all'anno attuale, allora calcolo etá, aggiorno variabile e mostro input nella card
+            age = currentYear - birthYearInput;
+            ageCard.innerHTML = `${age}`;
+        }
 
-    // mostro input utente nella card
-    ageCard.innerHTML = `${age}`;
-    });
+        else { // se l'utente inserisce piú o meno di 4 cifre, oppure un anno successivo all'attuale, svuoto il mio output nella card
+            age = null;
+            ageCard.innerHTML = "";
+        }
+    }
 
-kmNum.addEventListener('input', inp2 => {
-    inp2.preventDefault()
-    
-    const kmNumInput = kmNum.value;
-
+    else if (i.target.id === "kmNum") { // se utente sta digitando l'input dei km
+    kmNumInput = kmNum.value;
     // mostro input utente nella card
     kmCard.innerHTML = `${kmNumInput}`;
+    }
+
     });
 
 
@@ -45,15 +52,9 @@ kmNum.addEventListener('input', inp2 => {
 form.addEventListener('submit', e => 
     {   // prevent default all'evento submit (NO ricarica pagina)
         e.preventDefault(); 
-        
-        // estrazione degli input
-        const birthYearInput = birthYear.value; 
-        const kmNumInput = kmNum.value;
-
-        // calcolo etá
-        const age = currentYear - birthYearInput; 
-
+        // definisco costante e chiamo funzione per calcolo per prezzo
         const ticketPrice = price(kmNumInput, age);
+        // mostro il prezzo finale nella card
         finalPriceCard.innerHTML = `€${ticketPrice}`; 
     });
 
@@ -61,8 +62,10 @@ form.addEventListener('submit', e =>
     function price(km, age) {
         let finalPrice;
         const priceStandard = 0.21
-        if (age < 0 || age > 100) alert("devi inserire l'anno di nascita");
-        if (km < 0 || km > 2000) alert("inserisci un numero di km valido");
+        // controlli di validazione
+        if (age < 0 || age > 120) alert("devi inserire l'anno di nascita");
+        if (km <= 0 || km > 2000) alert("inserisci un numero di km valido");
+        // funzione
         else if (age < 18) finalPrice = priceStandard * 0.8 * km;
         else if (age > 65) finalPrice = priceStandard * 0.6 * km;
         else finalPrice = priceStandard * km;
